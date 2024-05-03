7de8cb0a37ae2590e9fbd2f8c7e7a5df3cd73a76
Clear008 <wissal-exo@hotmail.com>
2024-05-04 00:26:16 +0100

    2-hbnb

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    function updateAmenitiesH4() {
+        const amenityNames = Object.values(selectedAmenity);
+        let text = amenityNames.join(', ');
+        if (text.length > 50) {
+            text = amenityNames.slice(0, 3).join(', ');
+            if (amenityNames.length > 3) {
+                text += '...'
+            }
+        }
+        $('.amenities h4').text(text);
+    }
+  
+    let selectedAmenity = {};
+  
+    $('input[type="checkbox"]').change(function () {
+        if (this.checked) {
+            selectedAmenity[$(this).data('id')] = $(this).data('name');
+        } else {
+            delete selectedAmenity[$(this).data('id')];
+        }
+        updateAmenitiesH4();
+    });
+  
+    // AJAX request to check API status
+    $.get('http://0.0.0.0:5001/api/v1/status/', function(data, status) {
+        if (status === 'success' && data.status === 'OK') {
+            $('#api_status').addClass('available');
+        } else {
+            $('#api_status').removeClass('available');
+        }
+    });
+  });
+  
